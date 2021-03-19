28affbd5784ed3c6c4251808f2fb9ba2ff2e341e
LazaroMPerez <lazpz61@gmail.com>
2021-03-19 18:04:40 -0400

    added catch funtion

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-// HomePage
- // allow for when someone clicks the login it goes to the login page.
-
-
-//Create Profile / Login
-/* Create Profile  - > allow for userinput to be saved into the backend once it is created.
-then route the user to be able to create their profile and bring them to their profile page.
-
-Login -> allow for the user to login into the page and see their profile page
-*/
-
-// Profile Page
-/* 
-Allow for the user to be able to add any books that they want to, and leave a personal review,
-
-a rating for the book, and the a recommend button.
-*/
-
-
 //create user grabs
 const createUsername = document.getElementById("createUsername");
 const pword = document.getElementById("pword");
@@ -26,6 +7,8 @@ const submitBtn = document.getElementById("submitBtn");
 const loginUsername = document.getElementById("loginUsername");
 const loginPassword =document.getElementById("loginPassword");
 const submitBtnLogin = document.getElementById("submitBtnLogin");
+// constant for API
+const url = 'https://mobookslazproblems-api.herokuapp.com/'
 
 
 
@@ -35,9 +18,41 @@ fetch('https://mobookslazproblems-api.herokuapp.com/')
 .then(response =>) response.json())
 .then(data => //placeholder)
 
+catch(functions(){
+//code input here
+});
+
+
+
+
+
+
+
+
+
 
 
 // Adding a User  to User Endpoints 
+
+//Create Profile / Login
+/* Create Profile  - > allow for userinput to be saved into the backend once it is created.
+then route the user to be able to create their profile and bring them to their profile page.
+
+Login -> allow for the user to login into the page and see their profile page
+*/
+
+
+
+
+// Profile Page
+/* 
+Allow for the user to be able to add any books that they want to, and leave a personal review,
+
+a rating for the book, and the a recommend button.
+*/
+
+
+
 
 
 // Adding a Book to Book Enpoints
@@ -46,7 +61,7 @@ fetch('https://mobookslazproblems-api.herokuapp.com/')
 
 
 
-// Functionalities 
+// Functionalities of the page
 
 
 
